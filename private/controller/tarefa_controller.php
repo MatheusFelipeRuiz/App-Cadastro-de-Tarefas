7165d9363cc4eeafdb29c195aa2493d78fd8416c
MatheusFelipeRuiz <matheus57.ruiz100@gmail.com>
2023-12-31 16:29:46 -0300

    Refatorando o cadastro no Tarefa Controller

--- private/controller/tarefa_controller.php
+++ private/controller/tarefa_controller.php
@@ -3,16 +3,30 @@ require_once '../private/model/Tarefa.php';
 require_once '../private/service/TarefaService.php';
 require_once '../private/config/ConexaoBD.php';
 
+
+
+
+$descricao = strlen(trim($_POST['tarefa'])) > 0 ? $_POST['tarefa'] : null;
+$tarefa = new Tarefa($descricao);
+$conexao = new ConexaoBD();
+$tarefaService = new TarefaService($conexao, $tarefa);
+
+$acao = $_GET['acao'];
+echo '<pre>';
+var_dump($_GET);
+
+echo '</pre>';
+$tipoErro = null;
 try {
-    $descricao = strlen(trim($_POST['tarefa'])) > 0 ? $_POST['tarefa'] : null;
-    $tarefa = new Tarefa($descricao);
-    $conexao = new ConexaoBD();
-
-    $tarefaService = new TarefaService($conexao, $tarefa);
-
-    $tarefaService->inserir();
-    header('Location: nova_tarefa.php?cadastrado=1');
-
+    switch ($acao) {
+        case 'cadastrar':
+            $tipoErro = 'cadastro';
+            $tarefaService->inserir();
+            header('Location: nova_tarefa.php?cadastrado=1');
+    }
 } catch (PDOException $erro) {
-    header('Location: nova_tarefa.php?cadastrado=0');
+    switch($tipoErro){
+        case 'cadastro':
+            header('Location: nova_tarefa.php?cadastrado=0');
+    }
 }
